a880b5ecf96338d5abeecf87fd78cd336d86b4a1
DonatasNoreika <donoras@gmail.com>
2025-11-06 18:08:34 +0200

    2: +Sukurti visus modelius pagal nurodytą programos DB struktūrą

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField()
+    price = models.FloatField()
+
+
+class Car(models.Model):
+    make = models.CharField()
+    model = models.CharField()
+    license_plate = models.CharField(max_length=10)
+    vin_code = models.CharField(max_length=20)
+    client_name = models.CharField()
+
+
+class Order(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
+    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField()
