2f5295225a582cb821efb262d2c68ce5499a02b4
DonatasNoreika <donoras@gmail.com>
2025-11-06 18:19:27 +0200

    2: +aprašyti modelių __str__

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -5,6 +5,8 @@ class Service(models.Model):
     name = models.CharField()
     price = models.FloatField()
 
+    def __str__(self):
+        return self.name
 
 class Car(models.Model):
     make = models.CharField()
@@ -13,13 +15,21 @@ class Car(models.Model):
     vin_code = models.CharField(max_length=20)
     client_name = models.CharField()
 
+    def __str__(self):
+        return f"{self.make} {self.model}"
+
 
 class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.car} ({self.date})"
 
 class OrderLine(models.Model):
     order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
     service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.service} - {self.quantity}"
