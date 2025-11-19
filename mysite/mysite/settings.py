0092f1d9b1c8a927c2697c7a4d55721efa9ef3f0
DonatasNoreika <donoras@gmail.com>
2025-11-19 18:06:25 +0200

    8: +Padaryti vartotojo registracijos formą pagal šioje pamokoje išmoktus žingsnius.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = ALLOWED_HOSTS
 INSTALLED_APPS = [
     'autoservice',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -174,3 +176,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
