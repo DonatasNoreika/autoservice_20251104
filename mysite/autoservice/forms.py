c8ce378145e02ecdf0ca5b7908ac1bea3fd2df24
DonatasNoreika <donoras@gmail.com>
2025-11-20 18:37:49 +0200

    10: +Pakoreguoti registraciją, kad registruotų naują vartotoją (CustomUser).

--- mysite/autoservice/forms.py
+++ mysite/autoservice/forms.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
 from .models import OrderComment
+from .models import CustomUser
 from django import forms
 
 class OrderCommentForm(forms.ModelForm):
     class Meta:
         model = OrderComment
         fields = ['content']
+
+
+class CustomUserCreateForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password1', 'password2']
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -5,9 +5,9 @@ from .models import Car, Service, Order, CustomUser
 from django.views import generic
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from .forms import OrderCommentForm
+from .forms import OrderCommentForm, CustomUserCreateForm
 
 def index(request):
     num_visits = request.session.get("num_visits", 1)
@@ -92,7 +92,7 @@ class UserOrderListView(LoginRequiredMixin, generic.ListView):
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreateForm
     template_name = "signup.html"
     success_url = reverse_lazy("login")
 
